05df1e27486b08c37cd1360187256dddd1cfb350
candytangsys <candytang1009@gmail.com>
2025-09-30 16:17:46 +0800

    Account

---
+++ account.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class account {
+     private String name;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+            return name;
+    }
+
+    public static void main (String []args ){
+        Scanner input = new Scanner(System.in);
+
+        account myAccount = new account();
+        System.out.printf("Initial name is: %s%n%n" ,myAccount.getName());
+
+        System.out.println("Please enter the name:");
+        String theName = input.nextLine();
+        myAccount.setName(theName);
+        System.out.println();
+
+        System.out.printf("Name in object myAccount is :%n%s%n", myAccount.getName());
+        input.close();
+    }
+}
